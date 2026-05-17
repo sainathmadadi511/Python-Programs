f6d6420902b53296a01dbf3679dc4b01ad0df37a
sainathmadadi511 <sainathmadadi511@gmail.com>
2026-05-16 20:17:22 -0500

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+# data types
+
+# integer
+a = 10
+print(a)
+
+#string
+name = 'sarah'
+print(name)
+
+#stringconcactination
+name = 'sarah'
+text = 'hello'
+greet = name + " " + text # space character to keep the space in between the texts
+print(greet)
