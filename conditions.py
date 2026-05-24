9e428fe70622434e67a3c6e97fbb6a4975dee403
sainathmadadi511 <sainathmadadi511@gmail.com>
2026-05-23 19:56:05 -0500

    conditions

---
+++ conditions.py
@@ -0,0 +1,26 @@
+# conditions - operators ( or, and, not)
+
+# or - atleast one condition or comparision needs to be true.
+# and - both the conditions need to be true.
+
+# temparature = 75;
+# forecast = "rainy";
+
+# if temparature >= 80 and forecast != "sunny":
+#     print("Go outside!")
+# else:
+#     print('Stay Inside')
+
+# temparature = 45;
+
+# if temparature >= 60 or temparature >= 90:
+#     print("Go Outside!")
+# else:
+#     print("Stay Inside!")
+
+# raining = True;
+
+# if not raining:
+#     print("Go outside");
+# else:
+#     print("Stay Inside");
