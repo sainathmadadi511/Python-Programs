907abdb7d6cdef267907bfa2879ebdc168cb7497
sainathmadadi511 <sainathmadadi511@gmail.com>
2026-05-28 19:33:18 -0500

    ListasvaluesinDictionary

---
+++ ForloopDict.py
@@ -0,0 +1,16 @@
+
+
+# menus = { 'Breakfast' : ['Egg SandWich', 'Bagel', 'Coffee'],
+# 'Lunch' : ['BLT', 'PB&J', 'Turkey Sandwich'],
+# 'Dinner' : ['Soup', 'Salad', 'Taco']} 
+
+# for name, menu in menus.items():
+#     print(name, ':', menu)
+
+menus = [ [ 'Egg Sandwich', 'Bagel', 'Coffee'],
+['BLT', 'PB&J', "Turkey"],
+['Soup', 'Salad', 'Taco']]
+
+for menu in menus:
+    print(menu)
+    
